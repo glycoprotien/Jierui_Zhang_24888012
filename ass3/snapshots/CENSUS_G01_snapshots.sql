2bc8104395e3457b34a3f74c49b83eeabf54910c
glycoprotien <1223759889@qq.com>
2023-10-25 15:31:55 +0800

    12

--- ass3/snapshots/CENSUS_G01_snapshots.sql
+++ ass3/snapshots/CENSUS_G01_snapshots.sql
@@ -2,10 +2,11 @@
 
     {{
         config(
+          target_database='postgres'
           target_schema='RAW',
           strategy='timestamp',
           unique_key='LGA_CODE_2016',
-          updated_at='updated_at'
+          updated_at='updated_at',
         )
     }}
 
