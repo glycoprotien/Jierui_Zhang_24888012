acf61fcde4131d5520753f60dec677d91f8ae5f4
glycoprotien <1223759889@qq.com>
2023-10-25 14:56:00 +0800

    H

--- ass3/snapshots/CENSUS_G01_snapshots.sql
+++ ass3/snapshots/CENSUS_G01_snapshots.sql
@@ -1,14 +1,14 @@
-{% snapshot CENSUS_G01_snapshots %}
+{% snapshot CENSUS_G01_timestamp %}
 
-{{
+    {{
         config(
           target_schema='RAW',
           strategy='timestamp',
-          unique_key='LGA_CODE_2016',
-          updated_at: 'updated_at',
+          unique_key='id',
+          updated_at='updated_at',
         )
     }}
 
-select * from {{ source('RAW', 'CENSUS_G01') }}
+    select * from {{ source('RAW', 'CENSUS_G01') }}
 
 {% endsnapshot %}
