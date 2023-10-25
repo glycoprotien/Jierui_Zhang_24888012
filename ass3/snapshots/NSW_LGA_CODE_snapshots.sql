088ac127591cacdc5ef0929e34085cef066a9464
glycoprotien <1223759889@qq.com>
2023-10-25 15:05:07 +0800

    123

--- ass3/snapshots/NSW_LGA_CODE_snapshots.sql
+++ ass3/snapshots/NSW_LGA_CODE_snapshots.sql
@@ -1,14 +1,14 @@
-{% snapshot NSW_LGA_CODE_snapshots %}
+{% snapshot NSW_LGA_CODE_timestamp %}
 
-{{
+    {{
         config(
           target_schema='RAW',
           strategy='timestamp',
           unique_key='LGA_CODE',
-          updated_at: 'updated_at',
+          updated_at='updated_at',
         )
     }}
 
-select * from {{ source('RAW', 'NSW_LGA_CODE') }}
+    select * from {{ source('RAW', 'NSW_LGA_CODE') }}
 
 {% endsnapshot %}
