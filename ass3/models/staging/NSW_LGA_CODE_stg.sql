d1da962037c54debb51d671cad0d11adb4adb04b
glycoprotien <1223759889@qq.com>
2023-10-28 20:29:30 +0800

    asd

--- ass3/models/staging/NSW_LGA_CODE_stg.sql
+++ ass3/models/staging/NSW_LGA_CODE_stg.sql
@@ -12,26 +12,5 @@ source  as (
 
 ),
 
-renamed as (
-    select
-        LGA_CODE,
-        LGA_NAME,
-        CASE
-    WHEN EXTRACT(EPOCH FROM dbt_valid_from)::INT IS NULL THEN '1900-01-01'::TIMESTAMP
-    ELSE dbt_valid_from
-END AS dbt_valid_from,
-    dbt_valid_to
-    from source
-),
 
-unknown as (
-    select
-        0 as LGA_CODE,
-        NULL as LGA_NAME 
-        '1900-01-01'::timestamp as dbt_valid_from,
-        null::timestamp as dbt_valid_to
-
-)
-select * from unknown
-union all
-select * from renamed
+select * from source
