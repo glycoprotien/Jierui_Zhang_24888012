72e4f4f3dc11246be549963f289e523286990dce
glycoprotien <1223759889@qq.com>
2023-10-28 13:27:58 +0800

    as

--- ass3/models/warehouse/dim_lga.sql
+++ ass3/models/warehouse/dim_lga.sql
@@ -1,11 +1,7 @@
-{{
-    config(
-        unique_key='LGA_NAME'
-    )
-}}
+
 
 
 select
-a.LGA_CODE, b.LGA_NAME, b.SUBURB_NAME
-from 
-{{ ref('NSW_LGA_CODE_stg') }} a left join {{ ref('NSW_LGA_SUBURB_stg') }} b  on a.LGA_NAME = b.LGA_NAME
+a.LGA_CODE, b.LGA_NAME, a.SUBURB_NAME
+from {{ ref('NSW_LGA_SUBURB_stg') }} a
+left join {{ ref('NSW_LGA_CODE_stg') }} b on a.LGA_NAME = b.LGA_NAME
