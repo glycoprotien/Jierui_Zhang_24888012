e30575a002e3ab39a62951f9997d08f50a57a63a
glycoprotien <1223759889@qq.com>
2023-10-28 13:28:43 +0800

    ad

--- ass3/models/warehouse/dim_lga.sql
+++ ass3/models/warehouse/dim_lga.sql
@@ -2,6 +2,6 @@
 
 
 select
-a.LGA_CODE, b.LGA_NAME, a.SUBURB_NAME
+b.LGA_CODE, b.LGA_NAME, a.SUBURB_NAME
 from {{ ref('NSW_LGA_SUBURB_stg') }} a
 left join {{ ref('NSW_LGA_CODE_stg') }} b on a.LGA_NAME = b.LGA_NAME
